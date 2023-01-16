0e969243d5be2bf3fe0b9b37b87221723cd926c7
Julien POIRIER <connexion@julienpoirier-webdev.com>
2023-01-16 08:31:30 +0100

    update hero style

--- src/components/hero/HeroSection.jsx
+++ src/components/hero/HeroSection.jsx
@@ -2,16 +2,16 @@ import { FULL_NAME } from "../../lib/config";
 
 export const HeroSection = () => {
   return (
-    <div className="relative">
+    <div className="relative flex-col m-auto items-center md:flex-row md:pr-[52px]">
       <img
         width={300}
         height={300}
         src="/images/avatar.jpg"
         alt="avatar"
-        className="rounded shadow-lg"
+        className="top-[-16px]  right-[16px] rounded mx-auto shadow-lg md:absolute  "
       />
       {/* Hero - Exercise*/}
-      <div className="">
+      <div className="relative z-40 flex flex-col gap-4 md:mr-32">
         {/* Hero - Exercise*/}
         <h1 className="text-6xl drop-shadow-[0_0px_20px_rgba(0,0,0,0.25)] md:text-8xl">
           I'm{" "}
@@ -19,7 +19,7 @@ export const HeroSection = () => {
             {FULL_NAME}
           </span>
         </h1>
-        <p className="max-w-xl text-xl">
+        <p className="max-w-xl text-xl ">
           <b>Apprenti React.</b> I’m a software developer that make thing on
           internet, very happy to see your here, place holder please fill
           something here please fill something here.
